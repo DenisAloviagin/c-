8dd6acc6e3abb0ab6611de23a2b3e08ea9bc6dbc
denis aloviagin <d.aloviagin@gmail.com>
2023-02-12 23:05:47 +0500

    seminar1

---
+++ task7/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int LastNamber = 0;
+if(N > 99 && N < 1000)
+{
+    LastNamber = N%10;
+    Console.Write(LastNamber);
+}
+else
+{
+    Console.Write("Некоректный ввод");
+}
