4465eef92f6ad679b10860296d124d4869c442e2
denis aloviagin <d.aloviagin@gmail.com>
2023-02-13 00:50:24 +0500

    seminar1

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if((number1 % 2) == 0)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
