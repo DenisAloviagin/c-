ce0d0e4c74fe704c7fa36bac3a1ec34c7b3962e1
denis aloviagin <d.aloviagin@gmail.com>
2023-02-13 00:01:41 +0500

    s1

---
+++ task22/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число a: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.Write(number1);
+}
+else
+{
+    Console.Write(number2);
+}
